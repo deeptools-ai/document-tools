fe6e79890dc01ce18b475a9c874b959aad7d2e9e
ChainYo <t.chaigneau.tc@gmail.com>
2022-06-19 09:52:18 +0200

    enhance base document class and tests

--- document_tools/documents/base.py
+++ document_tools/documents/base.py
@@ -14,18 +14,96 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Union
 
 logger = logging.getLogger(__name__)
+
+
+DOCUMENT_EXTENSIONS = [
+    "ai",
+    "bmp",
+    "eps",
+    "gif",
+    "jpg",
+    "jpeg",
+    "pdf",
+    "png",
+    "psd",
+    "raw",
+    "svg",
+    "tif",
+    "tiff",
+    "webp",
+]
 
 
 @dataclass
 class Document:
     """
     Base class for all documents.
+
+    Parameters
+    ----------
+    path : str or Path
+        Path to the document.
+
+    Attributes
+    ----------
+    file : str
+        Name of the document.
+    extension : str
+        Extension of the document.
+
+    Raises
+    ------
+    ValueError
+        If the extension of the document is not valid. Valid extensions are ai, bmp, eps, gif, jpg, jpeg, pdf, png, psd,
+        raw, svg, tif, tiff and webp.
+
+    Examples
+    --------
+    >>> from document_tools.documents.base import Document
+    >>> document = Document("my_image.jpg")
+    >>> document.file
+    'my_image.jpg'
+    >>> document.extension
+    'jpg'
     """
 
-    file: str
+    _path: Union[str, Path] = field(repr=False)
+    file: str = field(init=False)
+    extension: str = field(init=False)
 
     def __post_init__(self):
+        if isinstance(self._path, str):
+            self._path = Path(self._path)
+
+        self.file = self._path.name
+
         self.extension = self.file.split(".")[-1]
+        if self.extension not in DOCUMENT_EXTENSIONS:
+            raise ValueError(
+                f"{self.extension} is not a valid extension. Valid extensions are: {', '.join(DOCUMENT_EXTENSIONS)}"
+            )
+
+
+@dataclass
+class ImageDocument(Document):
+    """
+    Class for image documents.
+    """
+
+    def __post_init__(self):
+        super().__post_init__()
+
+
+@dataclass
+class PDFDocument(Document):
+    """
+    Class for pdf documents.
+    """
+
+    def __post_init__(self):
+        super().__post_init__()
--- tests/documents/test_base.py
+++ tests/documents/test_base.py
@@ -13,40 +13,71 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from pathlib import Path
+from typing import List
+
 import pytest
 
 from document_tools.documents.base import Document
 
 
 @pytest.fixture
-def file_names():
-    return ["file1.txt", "file2.txt", "my_image.png", "my_favorite_file.pdf", "my.other.file.pdf"]
+def correct_paths():
+    return [
+        "my_image.ai",
+        "my_image.bmp",
+        "my_image.eps",
+        "my_image.gif",
+        "my_image.jpg",
+        "my_image.jpeg",
+        "my_image.pdf",
+        "my_image.png",
+        "my_image.psd",
+        "my_image.raw" "my_image.svg",
+        "my_image.tif",
+        "my_image.tiff",
+        "my_image.webp",
+        "my_favorite_file.pdf",
+        "my.other.file.pdf",
+        "/home/user/file.jpeg",
+        "home/username/file.tiff",
+    ]
 
 
-def test_document_base_class(file_names):
+@pytest.fixture
+def incorrect_paths():
+    return ["file1.txt", "/home/user/file.txt", "Desktop/document.sgi", "user.document.pcx"]
+
+
+def test_document_base_class(correct_paths: List[str], incorrect_paths: List[str]):
     """
     Test the base class for all documents.
     """
-    for file_name in file_names:
-        document = Document(file_name)
-        assert document.file == file_name
-        assert document.extension == file_name.split(".")[-1]
+    for path in correct_paths:
+        document = Document(path)
+        assert document._path == Path(path)
+        assert document.file == path.split("/")[-1]
+        assert document.extension == path.split(".")[-1]
+
+    for path in incorrect_paths:
+        with pytest.raises(ValueError):
+            Document(path)
 
 
-def test_document_repr(file_names):
+def test_document_repr(correct_paths: List[str]):
     """
     Test the representation of a document.
     """
-    for file_name in file_names:
-        document = Document(file_name)
-        assert repr(document) == f"Document(file='{file_name}')"
+    for path in correct_paths:
+        document = Document(path)
+        assert repr(document) == f"Document(file='{path.split('/')[-1]}', extension='{path.split('.')[-1]}')"
 
 
-def test_document_eq(file_names):
+def test_document_eq(correct_paths: List[str]):
     """
     Test the equality of two documents.
     """
-    for file_name in file_names:
-        document1 = Document(file_name)
-        document2 = Document(file_name)
+    for path in correct_paths:
+        document1 = Document(path)
+        document2 = Document(path)
         assert document1 == document2
