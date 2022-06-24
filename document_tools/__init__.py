c4f081971894f513d33e01887fc7ab2326f1fb19
ChainYo <t.chaigneau.tc@gmail.com>
2022-06-24 10:09:39 +0200

    fix version

--- document_tools/__init__.py
+++ document_tools/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for Document Tools."""
 
 __author__ = """deeptools.ai"""
-__email__ = 'contact@deeptools.ai'
-__version__ = '0.2.0'
+__email__ = "contact@deeptools.ai"
+__version__ = "0.0.1"
 
 
 from .encoders import TARGET_MODELS, LayoutLMv2Encoder, LayoutLMv3Encoder, LayoutXLMEncoder
